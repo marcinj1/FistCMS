6e35b3b7e2a87831bcd3b1ba1eab68a3f9f6b56f
marcinj <marcin.j120@gmail.com>
2013-06-06 22:48:06 +0200

    Master

---
+++ application/logs/2013/06/06.php
@@ -0,0 +1,17 @@
+<?php defined('SYSPATH') OR die('No direct script access.'); ?>
+
+2013-06-06 15:34:29 --- CRITICAL: Kohana_Exception [ 0 ]: A valid cookie salt is required. Please set Cookie::$salt. ~ SYSPATH\classes\Kohana\Cookie.php [ 152 ] in E:\serwer\htdocs\fist\system\classes\Kohana\Cookie.php:67
+2013-06-06 15:34:29 --- DEBUG: #0 E:\serwer\htdocs\fist\system\classes\Kohana\Cookie.php(67): Kohana_Cookie::salt('PHPSESSID', NULL)
+#1 E:\serwer\htdocs\fist\system\classes\Kohana\Request.php(155): Kohana_Cookie::get('PHPSESSID')
+#2 E:\serwer\htdocs\fist\index.php(117): Kohana_Request::factory(true, Array, false)
+#3 {main} in E:\serwer\htdocs\fist\system\classes\Kohana\Cookie.php:67
+2013-06-06 22:36:47 --- CRITICAL: Kohana_Exception [ 0 ]: A valid cookie salt is required. Please set Cookie::$salt. ~ SYSPATH\classes\Kohana\Cookie.php [ 152 ] in E:\serwer\htdocs\fist\system\classes\Kohana\Cookie.php:67
+2013-06-06 22:36:47 --- DEBUG: #0 E:\serwer\htdocs\fist\system\classes\Kohana\Cookie.php(67): Kohana_Cookie::salt('PHPSESSID', NULL)
+#1 E:\serwer\htdocs\fist\system\classes\Kohana\Request.php(155): Kohana_Cookie::get('PHPSESSID')
+#2 E:\serwer\htdocs\fist\index.php(117): Kohana_Request::factory(true, Array, false)
+#3 {main} in E:\serwer\htdocs\fist\system\classes\Kohana\Cookie.php:67
+2013-06-06 22:36:49 --- CRITICAL: Kohana_Exception [ 0 ]: A valid cookie salt is required. Please set Cookie::$salt. ~ SYSPATH\classes\Kohana\Cookie.php [ 152 ] in E:\serwer\htdocs\fist\system\classes\Kohana\Cookie.php:67
+2013-06-06 22:36:49 --- DEBUG: #0 E:\serwer\htdocs\fist\system\classes\Kohana\Cookie.php(67): Kohana_Cookie::salt('PHPSESSID', NULL)
+#1 E:\serwer\htdocs\fist\system\classes\Kohana\Request.php(155): Kohana_Cookie::get('PHPSESSID')
+#2 E:\serwer\htdocs\fist\index.php(117): Kohana_Request::factory(true, Array, false)
+#3 {main} in E:\serwer\htdocs\fist\system\classes\Kohana\Cookie.php:67
